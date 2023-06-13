ca58bd0cb91c1fdf53839a316b3a9a98a8a7e8fe
dev <dev@git.com>
2023-06-13 04:11:58 +0000

    middleware: Add Http3AltSvc config

--- middleware/http3_alt_svc.go
+++ middleware/http3_alt_svc.go
@@ -1,14 +1,20 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/skerkour/golibs/httputils"
 )
 
-func Http3AltSvc(next http.Handler) http.Handler {
+func Http3AltSvc(next http.Handler, port *string) http.Handler {
+	portStr := "443"
+	if port != nil {
+		portStr = *port
+	}
+	headerValue := fmt.Sprintf(`h3=":%s"; ma=86400, h3-29=":%s"; ma=86400`, portStr, portStr)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set(httputils.HeaderAltSvc, `h3=":443"; ma=86400, h3-29=":443"; ma=86400`)
+		w.Header().Set(httputils.HeaderAltSvc, headerValue)
 		next.ServeHTTP(w, r)
 	})
 }
